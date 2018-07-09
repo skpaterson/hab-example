7c90f696f6e621ba0f07a53193edce7e06dd5c6f
Stuart Paterson <spaterson@chef.io>
2018-07-09 10:28:28 +0100

    Debugging deployment.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- wordpress/plan.sh
+++ wordpress/plan.sh
@@ -18,13 +18,15 @@ pkg_svc_group=$pkg_svc_user
 
 pkg_deps=(core/nginx core/curl core/mysql-client)
 
-pkg_exports=()
-pkg_exposes=()
+pkg_exports=(
+   [port]=http.listen.port
+   [local_only]=http.listen.local_only
+)
 
 pkg_binds=(
   [database]="host port username password database"
+  [php]="port database"
 )
-
 
 do_build(){
   return 0
